d11803d19fbf9c14888885caf8eb4d41184e048f
mertoz41 <mertozka41@gmail.com>
2020-11-26 11:28:30 -0500

    about section updated

--- src/components/aboutsect.js
+++ src/components/aboutsect.js
@@ -7,11 +7,11 @@ export class Aboutsect extends Component {
             <div className={sectionStyle.container}>
                 <div className={sectionStyle.about}>
                     <p>I am a full-stack web developer with a background in international business studies and professional sports.
-                        Always fascinated with computers, I have transitioned to programming to control them. 
+                        Experienced in object-oriented programming, I strive to write efficient and readable code.  
                         Upon completing a 15-week software engineering bootcamp at the Flatiron School, I am specialized in Ruby and JavaScript frameworks.
                         As a full-stack, I enjoy working on the front-end to design and develop the user experience.
                         I am a rigorous developer, with a passion for problem solving and life-long learning.
-                        I bring strong collaboration and communication skills from playing 4 years of college and a year of professional basketball. 
+                        I bring strong collaboration, communication, and leadership skills gained from playing years of basketball, which translate well in Agile environments. 
     
                     </p>
                 </div>
