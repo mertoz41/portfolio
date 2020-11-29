bc12d757a7a6374716990542665c8658d85e928b
mertoz41 <mertozka41@gmail.com>
2020-11-29 16:05:05 -0500

    new about section, chomepage tab components readjusted.

--- src/components/aboutsect.js
+++ src/components/aboutsect.js
@@ -6,13 +6,19 @@ export class Aboutsect extends Component {
         return (
             <div className={sectionStyle.container}>
                 <div className={sectionStyle.about}>
-                    <p>I am a full-stack web developer with a background in international business studies and professional sports.
-                        Experienced in object-oriented programming, I strive to write efficient and readable code.  
-                        Upon completing a 15-week software engineering bootcamp at the Flatiron School, I am specialized in Ruby and JavaScript frameworks.
-                        As a full-stack, I enjoy working on the front-end to design and develop the user experience.
-                        I am a rigorous developer, with a passion for problem solving and life-long learning.
-                        I bring strong collaboration, communication, and leadership skills gained from playing years of basketball, which translate well in Agile environments. 
-    
+
+                    
+                    <p>Born and raised in Izmir, Turkey, I came to the US in 2010, 
+                        seeking to continue my education as a student-athlete. 
+                        Upon graduating from T.C. Williams High School in 2014 and 
+                        Thiel College in 2018 with a bachelors degree in International Business studies, 
+                        I played professional basketball for a year with Sigortam Net ITU Basket in Istanbul. 
+                        In 2020, I pursued my passion for technology by attending a software engineering 
+                        bootcamp at Flatiron School to learn full-stack web development. So far it has 
+                        been a smooth transition as I channel the discipline and work ethic gained from 
+                        basketball into this nonstop evolving industry. I spend most of my time working 
+                        on projects, studying computer science fundamentals, and adopting new technologies 
+                        in quest to mastery.                     
                     </p>
                 </div>
             </div>
